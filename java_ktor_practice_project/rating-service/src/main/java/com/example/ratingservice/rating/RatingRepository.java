253daded7e4ad2ebed82d8fc3dfd58f3d2cf0d0b
ileztom <91593668+ileztom@users.noreply.github.com>
2026-03-02 11:40:09 +0300

    Delete java_ktor_practice_project directory

--- java_ktor_practice_project/rating-service/src/main/java/com/example/ratingservice/rating/RatingRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.ratingservice.rating;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RatingRepository extends JpaRepository<Rating, Long> {
-}
